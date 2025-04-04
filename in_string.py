ceff2f01ac91ee4bef1900d9cf8eec18ead17c02
catamontaldo <cmontaldo@mail.austral.edu.ar>
2025-04-04 12:35:39 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,11 @@
 def check_vowels():
+    name = input(" ")
+    print(f"Contiene a: {'a' in name.lower()}")
+    print(f"Contiene e: {'e' in name.lower()}")
+    print(f"Contiene i: {'i' in name.lower()}")
+    print(f"Contiene o: {'o' in name.lower()}")
+    print(f"Contiene u: {'u' in name.lower()}")
+    
     # Código a implementar utilizando input.
 
 
